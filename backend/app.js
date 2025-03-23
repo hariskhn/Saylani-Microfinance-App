125017460d00d6175a8d72e79c41c5cca9fc6916
hariskhn <harislif3@gmail.com>
2025-03-23 06:50:30 +0500

    Add user routes

--- backend/app.js
+++ backend/app.js
@@ -12,4 +12,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
 
+import userRoutes from "./routes/user.route.js";
+
+app.use("/api/v1/user", userRoutes);
+
 export { app };
---
+++ backend/routes/user.route.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { registerUser, loginUser, logoutUser, forgotPassword} from "../controllers/user.controller.js"
+import { protectRoute } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.route('/signup').post( registerUser );
+router.route('/login').post( loginUser );
+router.route('/logout').post( protectRoute, logoutUser );
+router.route('/forgot-password').post( protectRoute, forgotPassword );
+// router.route('/refresh-token').post( refreshToken );
+
+export default router;
