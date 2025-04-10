8285e56fd4e1e87e9b77d67fd344dd6f136d6f0a
hariskhn <harislif3@gmail.com>
2025-04-10 19:42:51 +0500

    Update App.jsx with routes and user store integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/LoginPage";
+import { useUserStore } from "./stores/useUserStore";
+import DashboardPage from "./pages/DashboardPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
+import { useEffect } from "react";
 
 const App = () => {
+  const { user } = useUserStore();
+
+  useEffect(()=>{},[user]);
+
   return (
-    <div className='text-2xl'>Hello World!</div>
+    <div className="min-h-screen overflow-hidden">
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/change-password" element={ <ChangePasswordPage /> } />
+        <Route path="/dashboard" element={ user?.isPasswordChanged ? <DashboardPage />  : <Navigate to="/change-password" />} />
+      </Routes>
+    </div>
   )
 }
 
