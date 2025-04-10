8d108c3f96c36c3e46996c6c2cffc2820c15b692
hariskhn <harislif3@gmail.com>
2025-04-10 19:37:18 +0500

    Add loan categories

---
+++ frontend/src/loanCategories.js
@@ -0,0 +1,6 @@
+export const loanCategories = {
+    "Wedding": { maxLoan: 500000, maxPeriod: 3, subcategories: ["Valima", "Furniture", "Valima Food", "Jahez"] },
+    "Home Construction": { maxLoan: 1000000, maxPeriod: 5, subcategories: ["Structure", "Finishing", "Loan"] },
+    "Business Startup": { maxLoan: 1000000, maxPeriod: 5, subcategories: ["Buy Stall", "Advance Rent", "Shop Assets", "Shop Machinery"] },
+    "Education": { maxLoan: Infinity, maxPeriod: 4, subcategories: ["University Fees", "Child Fees Loan"] }
+};
