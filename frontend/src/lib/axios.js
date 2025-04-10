143b96af2a9370c73f0d505bbe685ac3d92c7fe2
hariskhn <harislif3@gmail.com>
2025-04-10 19:34:30 +0500

    Simplify axios baseURL config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api/v1" : import.meta.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true
 });
 
