58b2b7277ce499cbe7f755fcbf734ffc33779238
hariskhn <harislif3@gmail.com>
2025-04-22 00:48:59 +0500

    Increase JSON and URL-encoded payload limits

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({limit: "10mb"}))
+app.use(express.urlencoded({extended: true, limit: "10mb"}))
 app.use(cookieParser())
 
 import userRoutes from "./routes/user.route.js";
