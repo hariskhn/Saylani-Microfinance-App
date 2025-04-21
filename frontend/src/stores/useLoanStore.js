2cb1f4c1fcd342daeac14095144657433aabd963
hariskhn <harislif3@gmail.com>
2025-04-22 00:59:12 +0500

    Add loan fetching and update methods to loan store

--- frontend/src/stores/useLoanStore.js
+++ frontend/src/stores/useLoanStore.js
@@ -5,11 +5,11 @@ export const useLoanStore = create((set, get) => ({
     loan: null,
 
     createLoan: ({ category, subCategory, amountRequested, loanPeriod, initialDeposit }) => {
-        set({ loan: { category, subCategory, amountRequested, loanPeriod, initialDeposit }});
+        set({ loan: { category, subCategory, amountRequested, loanPeriod, initialDeposit } });
         console.log(get().loan);
     },
 
-    saveLoanInDB: async() => {
+    saveLoanInDB: async () => {
         try {
             console.log(get().loan);
             const res = await axios.post("/loan", get().loan);
@@ -17,5 +17,35 @@ export const useLoanStore = create((set, get) => ({
         } catch (error) {
             console.error(error);
         }
+    },
+
+    fetchAUsersLoans: async () => {
+        try {
+            const res = await axios.get("/loan");
+            console.log(res.data);
+            return res.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    fetchALoan: async ({ loanID }) => {
+        try {
+            const res = await axios.get(`/loan/${loanID}`);
+            console.log(res.data);
+            // return res.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    updateLoanRequest: async ({ loanID, guarantors, statementImg, salarySheetImg }) => {
+        try {
+            const res = await axios.patch(`/loan/${loanID}`, {guarantors, statementImg, salarySheetImg});
+            console.log(res.data);
+        } catch (error) {
+            console.error(error)
+        }
     }
+
 }))
